0a4f23323313de461a87c419fabbe9db8e74d642
aswinbalajig <aswinbalaji.gurumani@gmail.com>
2024-10-21 05:35:22 +0530

    changed db

--- main/models.py
+++ main/models.py
@@ -7,6 +7,4 @@ class rules(models.Model):
     rule_string=models.TextField()
     rule_ast=models.JSONField()
 
-class operands(models.Model):
-    rule=models.ForeignKey(rules,on_delete=models.CASCADE)
-    name=models.CharField(max_length=255)
+
